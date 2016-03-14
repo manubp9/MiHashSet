3d844f759fec40be70035ccc05346100b4739b17
Manuel <manubp9@gmail.com>
2016-03-14 12:31:14 +0100

    creada clase MiHashSet y creado su constructor

---
+++ MiHashSet.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+/**
+ * Write a description of class MiHashSet here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MiHashSet
+{
+    
+    // lista de enteros
+    private ArrayList lista;
+
+    /**
+     * Constructor for objects of class MiHashSet
+     */
+    public MiHashSet()
+    {
+        lista = new ArrayList();
+    }
+
+}
